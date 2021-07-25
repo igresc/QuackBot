a92271c017288f2b1c369ea39dd0184d44c1b19c
igresc <sergicastro2001@gmail.com>
2021-07-25 21:44:03 +0200

    Added some comments and refactor command to slash

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from discord_slash.utils.manage_commands import create_choice, create_option
 import asyncio
 import time
 import requests
+from requests.sessions import SessionRedirectMixin
 from sound import joke_to_sound_file
 
-client = discord.Client()
+# client = discord.Client()
 bot_token = "***REMOVED***" # Bot secret token https://discord.com/developers/applications
 
-#           EscuadronPZON           GAMBLING
+##### Guild server Ids #####
+#            EscuadronPZON           GAMBLING
 guild_ids=[265806519583506432, 494520437603172362]
 
 bot = commands.Bot(command_prefix='/')
@@ -20,6 +22,14 @@ slash = SlashCommand(bot, sync_commands=False)
 dadjoke_url = "https://icanhazdadjoke.com/"
 headers = {'Accept': 'application/json', 'User-Agent': 'Api tests (sergicastro2001@gmail.com)'}
 
+##### Event on_ready() to know when the bot is ready and functional #####
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+    # print('We have logged in as {0.user}'.format(client))
+
+
+##### Ping #####
 @slash.slash(
     name="ping",
     description="Ping QuackBot",
@@ -28,24 +38,24 @@ headers = {'Accept': 'application/json', 'User-Agent': 'Api tests (sergicastro20
 async def _ping(ctx:SlashContext):
     await ctx.send("Pong!")
 
-@bot.event
-async def on_ready():
-    print("Bot is ready!")
-    # print('We have logged in as {0.user}'.format(client))
-
-@bot.command()
-async def joke(ctx):
+##### Joke Text #####
+@slash.slash(
+    name="joke",
+    description="Tells a dad joke.",
+    guild_ids=guild_ids
+)
+async def _joke(ctx):
     r = requests.get(url=dadjoke_url, headers=headers)
     joke = r.json()
     await ctx.send("{}".format(joke["joke"]))
     return joke["joke"]
 
-# @bot.command()
-# async def ping(ctx):
-#     user_id = ctx.message.author.id
-#     await ctx.send("<@%s> Pong!" % user_id)
-
-@bot.command()
+##### Ban of life #####
+@slash.slash(
+    name="banoflife",
+    description="Ban someone of life in a perfect grammar.",
+    guild_ids=guild_ids
+)
 async def banoflife(ctx):
     args = ctx.message.content.split(" ")
     if len(args) >= 2:
@@ -56,17 +66,25 @@ async def banoflife(ctx):
         await ctx.message.channel.send("<@{}> asi do vaneado dela bida".format(userID), tts=True)
         print(userID)
 
+##### Join authors voice Channel #####
 @bot.command(name="joina")
 async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
+    try:
+        channel = ctx.author.voice.channel
+        try:
+            await channel.connect()
+        except discord.errors.ClientException:
+            print("Error")
+    except AttributeError:
+        await ctx.send("<@{}> You need to be in a voice channel".format(ctx.author.id))
+
     #await ctx.send("Joined the voice channel {}".format(ctx.author.voice.channel))
 
 @bot.command(name="leavea")
 async def leave(ctx):
     await ctx.voice_client.disconnect()
 
-#@bot.command()
+##### Joke TTS #####
 @slash.slash(
     name="joke_tts",
     description="Tells you a dad joke into your ear.",
@@ -80,7 +98,7 @@ async def _joke_tts(ctx):
         await join(ctx)
         guild = ctx.guild
         voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
-    joke_text = await joke(ctx)
+    joke_text = await _joke(ctx)
     joke_to_sound_file(joke_text)
     audio_source = discord.FFmpegPCMAudio('joke.mp3')
 
@@ -100,9 +118,9 @@ async def _joke_tts(ctx):
 #     print("UserID", userID)
 #     print("Message", message.content)   
 
-@bot.listen
-async def on_message(message):
-    print(message)
+# @bot.listen
+# async def on_message(message):
+#     print(message)
     
 bot.run(bot_token)
-client.run(bot_token)
+# client.run(bot_token)

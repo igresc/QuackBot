4c8790d533a4e480a1f24cf8f8d3f7d3d3af3b6e
igresc <sergicastro2001@gmail.com>
2021-08-03 21:06:57 +0200

    Extracted some functions to extra_functions.py

--- src/extra_functions.py
+++ src/extra_functions.py
@@ -1,4 +1,19 @@
 import os, requests, random, json
+from gtts import gTTS
+
+def string_to_sound_file(string_text, file_name):
+    # Language in which you want to convert
+    language = 'en'
+
+    # Passing the text and language to the engine, 
+    # here we have marked slow=False. Which tells 
+    # the module that the converted audio should 
+    # have a high speed
+    myobj = gTTS(text=string_text, lang=language, slow=False)
+
+    # Saving the converted audio in a mp3 file named
+    # welcome 
+    myobj.save(file_name)
 
 def tenor_get():
     TENOR_API_KEY = os.environ["TENOR_API_KEY"]
@@ -17,4 +32,16 @@ def tenor_get():
     random_gif = random.choice(results)
     return random_gif["url"]
 
-# print(tenor_get())
+def joke_request():
+    # icanhazdadjoke.com api definitions
+    dadjoke_url = "https://icanhazdadjoke.com/"
+    headers = {'Accept': 'application/json', 'User-Agent': 'Api tests (sergicastro2001@gmail.com)'}
+    r = requests.get(url=dadjoke_url, headers=headers)
+    joke = r.json()["joke"]
+    return joke
+
+def rm_file(filename):
+    try:
+        os.remove(filename)
+    except PermissionError as e:
+        print(e)
--- src/main.py
+++ src/main.py
@@ -3,10 +3,8 @@ from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_option
 import requests, os
-from sound import string_to_sound_file
 import extra_functions as ext
 
-# client = discord.Client()
 bot_token = os.environ["BOT_TOKEN"] # Bot secret token https://discord.com/developers/applications
 
 ##### Guild server Ids #####
@@ -16,15 +14,11 @@ guild_ids=[265806519583506432, 494520437603172362]
 bot = commands.Bot(command_prefix='/')
 slash = SlashCommand(bot, sync_commands=False)
 
-# icanhazdadjoke.com api definitions
-dadjoke_url = "https://icanhazdadjoke.com/"
-headers = {'Accept': 'application/json', 'User-Agent': 'Api tests (sergicastro2001@gmail.com)'}
 
 ##### Event on_ready() to know when the bot is ready and functional #####
 @bot.event
 async def on_ready():
     print("Bot is ready!")
-    # print('We have logged in as {0.user}'.format(client))
 
 
 ##### Ping #####
@@ -39,10 +33,9 @@ async def _ping(ctx:SlashContext):
 ##### Joke Text #####
 @bot.command(name="joke")
 async def _joke(ctx):
-    r = requests.get(url=dadjoke_url, headers=headers)
-    joke = r.json()["joke"]
-    await ctx.send(joke)
-    return joke
+    joke_text = ext.joke_request()
+    await ctx.send(joke_text)
+    return joke_text
 
 ##### Ban of life #####
 @slash.slash(
@@ -81,11 +74,6 @@ async def join(ctx):
 async def leave(ctx):
     await ctx.voice_client.disconnect()
 
-def rm_joke(filename):
-    try:
-        os.remove(filename)
-    except PermissionError as e:
-        print(e)
 
 ##### Joke TTS #####
 @slash.slash(
@@ -100,11 +88,11 @@ async def _joke_tts(ctx):
 
     joke_text = await _joke(ctx)
     filename="data/joke.mp3"
-    string_to_sound_file(joke_text, filename)
+    ext.string_to_sound_file(joke_text, filename)
     audio_source = discord.FFmpegPCMAudio(filename)
 
     if not voice_client.is_playing():
-        voice_client.play(audio_source, after=lambda _: rm_joke(filename))
+        voice_client.play(audio_source, after=lambda _: ext.rm_file(filename))
 
 ##### Quack Voice #####
 @slash.slash(
@@ -129,26 +117,9 @@ async def _quack(ctx):
     description="Gifs you a GIF",
     guild_ids=guild_ids
 )
-async def duck(ctx):
+async def _duck(ctx):
     gif = ext.tenor_get()
     await ctx.send(gif)
 
-# @client.event
-# async def on_message(message):
-#     userID = message.author.id
-#     #if userID == 301430727008976908:
-#     if userID == 257592218888568832:
-#         await message.channel.send("<@%s> calla puto." % userID)
-#         await message.delete()
-
-#     # channel = message.channel
-#     # print("Channel", channel)
-#     print("UserID", userID)
-#     print("Message", message.content)   
-
-# @bot.listen
-# async def on_message(message):
-#     print(message)
     
 bot.run(bot_token)
-# client.run(bot_token)
